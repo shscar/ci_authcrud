84401f3638976de3e77f05dc59d933007ee35281
shscar <ahmadghozali87621@gmail.com>
2023-04-11 16:52:38 +0700

    setup4

--- app/Views/crud/create.php
+++ app/Views/crud/create.php
@@ -2,25 +2,47 @@
 
 <?= $this->section('content') ?> 
 
-    <div class="row justify-content-center">
-        <div class="col-md-6 text-center mb-5">
-            <h2 class="heading-section">Create Data Siswa</h2>
-        </div>
-        <form action="">
+    <div class="container mt-5">
+        <h1 class="mb-4 text-center">Form Tambah Siswa</h1>
+        <form action="<?= route_to('storeJurusan') ?>" method="POST">
+            <?= csrf_field(); ?>
+
             <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Email address</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                <!-- <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div> -->
+                <label for="nama" class="form-label">NISN</label>
+                <input type="number" name="nisn" class="form-control" id="nisn" placeholder="9736" required>
             </div>
             <div class="mb-3">
-                <label for="exampleInputPassword1" class="form-label">Password</label>
-                <input type="password" class="form-control" id="exampleInputPassword1">
+                <label for="nama" class="form-label">Nama Lengkap</label>
+                <input type="text" name="nama" class="form-control" id="nama" placeholder="Masukkan nama Lengkap" required>
             </div>
-            <div class="mb-3 form-check">
-                <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+            <div class="mb-3">
+                <label for="jk" class="form-label">Jenis Kelamin</label>
+                <select name="jk" class="form-select" aria-label="Default select example">
+                    <option selected>Pilih Jenis kelamin</option>
+                    <option value="Perempuan">Perempuan</option>
+                    <option value="Laki-laki">Laki-laki</option>
+                </select>
             </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
+            <div class="mb-3">
+                <label for="kelas" class="form-label">Kelas</label>
+                <input type="text" name="kelas" class="form-control" id="kelas" placeholder="Kelas" required>
+            </div>
+            <div class="mb-3">
+                <label for="jurusan" class="form-label">Jurusan</label>
+                <input type="text" name="jurusan" class="form-control" id="jurusan" placeholder="Jurusan" required>
+            </div>
+            <div class="mb-3">
+                <label for="tlp" class="form-label">No. Telepon</label>
+                <input type="number" name="no_tlp" class="form-control" id="tlp" placeholder="0812345678900" required>
+            </div>
+            <!-- <div class="mb-3">
+                <label for="deskripsi" class="form-label">Deskripsi</label>
+                <textarea class="form-control" id="deskripsi" rows="3" placeholder="Masukkan deskripsi siswa"></textarea>
+            </div> -->
+            <div class="text-end">
+                <a href="javascript:history.back()" class="btn btn-outline-secondary mr-2" role="button">Batal</a>
+                <button type="submit" class="btn btn-primary">Simpan</button>
+            </div>
         </form>
     </div>
 
--- app/Views/index.php
+++ app/Views/index.php
@@ -11,29 +11,31 @@
     <div class="row">
         <div class="col-md-12">
             <div class="table-wrap">
-                <a href="<?= route_to('tambah-siswa') ?>" class="btn btn-primary">Tambah Data Siswa</a>
+                <a href="<?= route_to('tambah-siswa') ?>" class="btn btn-success">Tambah Data Siswa</a>
                 <table class="table table-striped text-center">
                     <thead>
                         <tr>
                             <th scope="col">No</th>
-                            <th scope="col">Nama</th>
                             <th scope="col">NISN</th>
-                            <th scope="col">Gender</th>
+                            <th scope="col">Nama</th>
+                            <th scope="col">Jenis Kelamin</th>
                             <th scope="col">Kelas</th>
-                            <th scope="col">Alamat</th>
+                            <th scope="col">Jurusan</th>
+                            <th scope="col">No.tlp</th>
                             <th scope="col">Aksi</th>
                         </tr>
                     </thead>
                     <tbody>
                         <tr>
                             <th scope="row">1</th>
-                            <td>Ali</td>
                             <td>3646</td>
-                            <td>L</td>
-                            <td>02</td>
-                            <td>Japan</td>
+                            <td>Verlina</td>
+                            <td>Perempuan</td>
+                            <td>11</td>
+                            <td>Rekayasa Perangkat Lunak</td>
+                            <td>0812345678964</td>
                             <td>
-                                <!-- <a href="#" class="btn btn-success">Progress</a> -->
+                                <!-- <a href="#" class="btn btn-success">Detail</a> -->
                                 <a href="#" class="btn btn-warning">Edit</a>
                                 <a href="#" class="btn btn-danger">Hapus</a>
                             </td>
@@ -43,6 +45,6 @@
             </div>
         </div>
     </div>
-    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    <br><br><br><br><br><br><br><br><br><br><br><br><br>
 
 <?= $this->endSection() ?>
